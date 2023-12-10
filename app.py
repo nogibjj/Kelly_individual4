1f052928262f7482fc5f3ea6c06f17c686d9da04
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-12-09 20:16:55 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+# Basic Website Template
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", title="Home")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About Us")
+
+
+@app.route("/api/openai")
+def prompt():
+    return render_template("prompt.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
